5b2539cc6fb85f8440212113fa12a119578dae05
GoTouchGra55 <shauryatamang.dev@gmail.com>
2026-08-21 22:13:08 +0545

    Make BMP580 work

--- firmware/Core/Inc/Drivers/BMP.h
+++ firmware/Core/Inc/Drivers/BMP.h
@@ -3,10 +3,21 @@
 
 #include <stdint.h>
 
-void BMP_Init(void);
+typedef enum {
+  STANDBY = 0x00, 
+  NORMAL = 0x01, 
+  FORCED = 0x02, 
+  NON_STOP = 0x03
+} BMP_PWR_MODE;
+
+void BMP_Init(BMP_PWR_MODE Power_mode, uint8_t OSR, uint8_t ODR);
 void BMP_CS_High(void);
 void BMP_CS_Low(void);
+uint8_t BMP_GetWhoami(void);
 
 uint8_t BMP_SPI2_Transfer(uint8_t data);
+void BMP_WriteReg(uint8_t reg, uint8_t data);
+float BMP_ReadPressureRaw(void);
+float BMP_ReadHeight(float GROUND_PRESSURE);
 
 #endif
--- firmware/Core/Src/Drivers/BMP.c
+++ firmware/Core/Src/Drivers/BMP.c
@@ -1,8 +1,15 @@
 #include "BMP.h"
 #include "Comms.h"
+#include "math.h"
 #include <stm32h743xx.h>
 
-void BMP_Init(void){
+void BMP_Init(BMP_PWR_MODE Power_mode, uint8_t OSR, uint8_t ODR){
+  uint8_t osr_config = (0x01 << 6) |       // press_en 
+                        (OSR << 3) |       // OSR_P = set by user 
+                        (0x00 << 0);       // OSR_T = 1x
+
+  uint8_t odr_config = (ODR << 2) | Power_mode;  
+
   // Enable clocks
   RCC->AHB4ENR |= RCC_AHB4ENR_GPIOBEN | RCC_AHB4ENR_GPIOCEN;
   
@@ -25,9 +32,22 @@ void BMP_Init(void){
   GPIOC->OTYPER &= ~GPIO_OTYPER_OT0;
   GPIOC->PUPDR &= ~GPIO_PUPDR_PUPD0;
   GPIOC->OSPEEDR |= GPIO_OSPEEDR_OSPEED0;
+
   BMP_CS_High(); // Start deselected
-  
   Comms_Init(BMP);
+
+  // DO NOT MOVE AHEAD OF COMMS_INIT! >:(
+  // Select NORMAL mode and ODR
+  BMP_WriteReg(0x37, odr_config);
+  // Set OSR
+  BMP_WriteReg(0x36, osr_config);
+}
+
+void BMP_WriteReg(uint8_t reg, uint8_t data){
+  BMP_CS_Low();
+  BMP_SPI2_Transfer(reg & 0x7F);
+  BMP_SPI2_Transfer(data);
+  BMP_CS_High();
 }
 
 uint8_t BMP_SPI2_Transfer(uint8_t data){
@@ -35,6 +55,39 @@ uint8_t BMP_SPI2_Transfer(uint8_t data){
   *(__IO uint8_t *)&SPI2->TXDR = data;
   while (!(SPI2->SR & SPI_SR_RXP)); // Wait for RXP to be available for RX ops
   return *(__IO uint8_t *)&SPI2->RXDR;
+}
+
+uint8_t BMP_GetWhoami(void){
+  // CHIP_ID
+  BMP_CS_Low();
+  BMP_SPI2_Transfer(0x81);
+  uint8_t id = BMP_SPI2_Transfer(0x00);
+  BMP_CS_High();
+  return id;
+}
+
+float BMP_ReadPressureRaw(void){
+  uint8_t buf[3];
+  BMP_CS_Low();
+  BMP_SPI2_Transfer(0x20 | 0x80);
+  buf[0] = BMP_SPI2_Transfer(0x00);
+  buf[1] = BMP_SPI2_Transfer(0x00);
+  buf[2] = BMP_SPI2_Transfer(0x00);
+  BMP_CS_High();
+  uint32_t raw_pressure =
+    ((uint32_t)buf[2] << 16) |
+    ((uint32_t)buf[1] << 8)  |
+    buf[0];
+  return raw_pressure / 64.0f;
+}
+
+float BMP_ReadHeight(float ground_pressure){
+    float pressure = BMP_ReadPressureRaw();
+    float ratio = powf(
+        pressure / ground_pressure,
+        1.0f / 5.255f
+    );
+    return 44330.0f * (1.0f - ratio);
 }
 
 void BMP_CS_High(){
